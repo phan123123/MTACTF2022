bdf319857051fec97946c57eed78b356ebb5b9c6
phan123123 <phan123123@gmail.com>
2022-08-05 17:59:29 -0700

    fix port for cookie chall

--- cookie/setup.sh
+++ cookie/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo docker build -t "cookie" . && sudo docker run -d -p "0.0.0.0:1337:1337" --cap-add=SYS_PTRACE --security-opt seccomp=unconfined cookie
+sudo docker build -t "cookie" . && sudo docker run -d -p "0.0.0.0:1338:1337" --cap-add=SYS_PTRACE --security-opt seccomp=unconfined cookie
